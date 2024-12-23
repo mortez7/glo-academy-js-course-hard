1ec25ebd17888c18d75e072493c5a71f48632f1b
Ivan Sokolenko <ivan.sokolenko.2017@gmail.com>
2024-12-23 11:48:32 +0200

    внесены исправления согласно условия задания

--- js/main.js
+++ js/main.js
@@ -12,10 +12,11 @@ const today = new Date();
 
 for (let key in week) {
   if (Number(key) === today.getDay() - 1) {
-    console.log(`%c${week[key]}`, "font-weight: bold;");
+    document.body.insertAdjacentHTML("beforeend", `<b>${week[key]}</b>`);
   } else if (Number(key) === 5 || Number(key) === 6) {
-    console.log(`%c${week[key]}`, "font-style: italic;");
+    document.body.insertAdjacentHTML("beforeend", `<i>${week[key]}</i>`);
   } else {
-    console.log(week[key]);
+    document.body.insertAdjacentHTML("beforeend", `${week[key]}`);
   }
+  document.body.insertAdjacentHTML("beforeend", "<br>");
 }
